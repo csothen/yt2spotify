4a53c448b04407a1f16928e2555182a5dcddbe0b
csothen <cesarjcpinheiro@gmail.com>
2021-12-28 00:07:19 +0000

    client: Add button to test Spotify authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const getAuthURL = () => {
+    return fetch("http://localhost:8080/auth/url")
+      .then((response) => response.json())
+      .then((data) => {
+        window.location.replace(data.url);
+      });
+  };
+
+  const isAuthenticated = () => {
+    fetch("http://localhost:8080/auth/spotify/check")
+      .then((response) => response.json())
+      .then((data) => {
+        setAuthenticated(data.status);
+        if (!data.status) {
+          getAuthURL();
+        }
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => isAuthenticated()}>Login with Spotify</button>
       </header>
     </div>
   );
